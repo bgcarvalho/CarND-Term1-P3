d7dffd06459c624ebbec9d9ac790c9897c1c2df5
Bruno Carvalho <bgcarvalho@gmail.com>
2017-10-25 18:17:47 -0200

    initial python files

---
+++ model.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+from keras import Input, Dense, Flatten
+from keras.models import load_model
+import h5py
+from keras import __version__ as keras_version
+
